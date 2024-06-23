34853e9c08336c5d9324be141b497fcdb4b1bbe2
__yun <ylw167@qq.com>
2024-06-24 07:49:15 +0800

    JiChao试卷

---
+++ src/main/java/teaching/system/backend/domain/dto/JiChaoTestDTO.java
@@ -0,0 +1,15 @@
+package teaching.system.backend.domain.dto;
+
+import lombok.Data;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: __yun
+ * @Date: 2024/06/24/7:30
+ * @Description:
+ */
+@Data
+public class JiChaoTestDTO {
+    private String testPaper;
+}
